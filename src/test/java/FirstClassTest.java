dc5bc9e71d8840e56b9335e4cac91d7086fb4d57
Anastasiya Bessmertnova <avbessmertnova@gmail.com>
2024-07-02 22:19:07 +0400

    add new test #2

--- src/test/java/FirstClassTest.java
+++ src/test/java/FirstClassTest.java
@@ -4,9 +4,22 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Selenide.$;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FirstClassTest {
+    MainPage mainPage = new MainPage();
+
+    @Test
+    public void search() {
+        mainPage.searchButton.click();
+
+        $("[data-test-id='search-input']").sendKeys("Selenium");
+        $("button[data-test='full-search-button']").click();
+
+        $("input[data-test-id='search-input']").shouldHave(attribute("value", "Selenium"));
+    }
 
     @Test
     void dataToday() {
