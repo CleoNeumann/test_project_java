70a7506583f02bbfce2cc9f3ea710663f78cf4c9
Anastasiya Bessmertnova <avbessmertnova@gmail.com>
2024-07-02 22:11:26 +0400

    add new test

--- src/test/java/FirstClassTest.java
+++ src/test/java/FirstClassTest.java
@@ -4,9 +4,19 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FirstClassTest {
+
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
 
     @Test
     void dataToday() {
